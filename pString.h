4525e4cfa56c8411b684c30322394b19e3107a84
jmnc2 <joseph@chamness.org>
2021-01-09 00:18:06 +0000

    small commit :P

---
+++ pString.h
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+// remember "a" is an array of char, char a[]; 'c' only works for 1 char not an array ie char c = 'c';
+// so any k value should be in this type of quotes "ab"
+// and any c value should be in this type of quotes 'a'
+
+//#define "\4GET "
+;
+void pPrint(const char s[]);
+void pPrintln(const char s[]);
+
+//{ the k String is looped through a char at a time }
+byte InStringS(const char s1[], const char s2[], boolean start = true);
+boolean StrCom(const char s1[], const char s2[] );// True if the string array have the same values. (i.e. s1[0..s1[0]] == s2[0..s1[0]] )
+byte FindFirstC(const char s[], const char c);
+byte FindFirstK(const char s[], const char k[]);
+
+void DelFirst(char s[], byte n);
+void DelToC(char s[], byte c);
+void DelToC1(char s[], byte c);
+
+#define SplitC1_(A,C,R) SplitC1(A, C, R, sizeof(R) - 1 );
+void Split1(char s[], byte n, char r[], byte rLength);
+#define SplitC2_(A,C,R) SplitC2(A, C, R, sizeof(R) - 1 );
+void SplitC2(char a[], char c, char r[], byte rLength);
+#define SplitK2_(A,C,R) SplitK2(A, C, R, sizeof(R) - 1 );
+void SplitK2(char a[], const char k[], char r[], byte rLength);
+
+#define SplitK1_(A,C,R) SplitK1(A, C, R, sizeof(R) - 1 );
+void SplitK1( char s[],  const char k[], char r[], byte rLength);
+
+
+;
---
+++ relays.h
@@ -0,0 +1,20 @@
+#ifndef _relays_h
+#define _relays_h
+
+#include <Arduino.h>
+
+
+#define no_of_relays 2
+#define initHLa ={0,0}
+#define relayPins 3,4
+;
+extern byte pinsA[no_of_relays];
+extern byte relayState[no_of_relays];// 0 = off, 1 = on
+extern byte relayOnVal[no_of_relays];// eg 0 for pull the relay LOW to turn on, 1 for pull high. Pull opisit to turn off. Or should it be set to input to turn off?
+
+void updateRelayState(byte n, byte v);
+void updateRelayState(byte key, char value[]);
+void updateRelayState(char key[], char value[]);
+
+
+#endif
