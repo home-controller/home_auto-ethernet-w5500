509a76cfc7662f1259a42b1cb89bbde83ce2f134
jmnc2 <joseph@chamness.org>
2021-01-19 16:31:56 +0000

    'Changed to put pin in INPUT mode to turn off'

--- git_commit.sh
+++ git_commit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #if argument 1 is not set or nul use default.
-pram1VAR=${0:-"A small update"}
+pram1VAR=${1:-"A small update"}
 git add .
 git status
-git commit -m $pram1VAR
+git commit -m "'$pram1VAR'"
+
 git push
+echo \"$pram1VAR\"
 
